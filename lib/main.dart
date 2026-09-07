3231c11abefa60d2ebd203c5cb51fd1b525486e9
Md. Sayeed Hassan Maheen <sayeedhassan1124@gmail.com>
2026-09-07 22:53:05 +0600

    Add Firebase Firestore and Storage dependencies, initialize Firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_admin/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,6 +10,8 @@ import 'core/bindings/initial_bindings.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final preferences = await SharedPreferences.getInstance();
 
